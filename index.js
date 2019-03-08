fbd3a1f9cfd398138d1f3ecea4a29f548ff47ce4
Michal Pekar <pekac45@gmail.com>
2019-03-08 23:44:37 +0000

    cleared up, amended to every 60 minutes, got rid of some console.logs

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 // const { meme } = require("./daily");
 const { downloadPicture } = require("./downloadPicture");
 const { tweet } = require("./tweet");
-let oldMeme;
+let oldMeme = "running 1st time.";
 
 const fetch = require("node-fetch");
 
 // const dailyLink = "https://www.reddit.com/r/memes/top.json?t=day";
 const meme = async () => {
 	try {
-		const res = await fetch("https://www.reddit.com/r/memes/top.json?t=hour");
+		const res = await fetch("https://www.reddit.com/r/memes/top.json?t=day");
 		if (res.status >= 400) {
 			throw new Error("Bad response from server");
 		}
@@ -28,42 +28,10 @@ const meme = async () => {
 			};
 			dailyArr.push(day);
 		}
-		console.log("TCL: meme -> dailyArr", dailyArr[0]);
 		return dailyArr;
 	} catch (err) {
 		console.error(err);
 	}
-	// #
-	// #
-	// #
-	// const reply = [
-	// 	{
-	// 		title: "It's our hero",
-	// 		rating: 11611,
-	// 		url: "https://i.redd.it/sq8huxjf6tk21.png",
-	// 		id: "ayl70912"
-	// 	},
-	// 	{
-	// 		title: "X:324 Y:75 Z:-678",
-	// 		rating: 11888,
-	// 		url: "https://i.redd.it/ynn4hwlxbvk21.png",
-	// 		id: "ayokxb"
-	// 	},
-	// 	{
-	// 		title: "Press F to pay respect.",
-	// 		rating: 11659,
-	// 		url: "https://i.redd.it/jx4co1x6muk21.jpg",
-	// 		id: "aynlrc"
-	// 	},
-	// 	{
-	// 		title: "And that's a fact",
-	// 		rating: 11394,
-	// 		url: "https://i.redd.it/otexb3ic5rk21.jpg",
-	// 		id: "ayh0e5"
-	// 	}
-	// ];
-	// console.log("TCL: meme -> reply", reply[0]);
-	// return reply;
 };
 
 function sleep(ms) {
@@ -71,10 +39,6 @@ function sleep(ms) {
 }
 
 const makeItHappen = async () => {
-	// Add if meme.id = previous.meme.id
-	// wait 30 minutes
-	// else do above
-	// meme().then(maymay[i].title);
 	meme()
 		.then(downloadPicture)
 		.then(meme)
@@ -84,42 +48,35 @@ const makeItHappen = async () => {
 const alreadyPosted = async () => {
 	let latestMeme = await meme();
 	let memeId = latestMeme[0].id;
-	await console.log(`memeId is ${memeId}`);
-	await console.log(`oldMeme is ${oldMeme}`);
-
-	// console.log(memeId !== oldMeme);
+	await console.log("Starting already posted? script...");
+	await console.log(
+		`Current top reddit meme is: ${memeId}. Last posted is: ${oldMeme}`
+	);
 
 	if (memeId !== oldMeme) {
-		// 	await makeItHappen();
-
-		console.log("tweeting from index.js TRUE PART");
 		oldMeme = memeId;
-		console.log("Taking a break...");
-		makeItHappen();
-		await sleep(1800000);
-		console.log("30 minutes later");
-
-		console.log(oldMeme, memeId);
+		await makeItHappen();
+		await console.log("Done posting.");
+		await console.log("Taking a break...");
+		await sleep(3600000);
+		console.log("60 minutes later");
 
 		alreadyPosted();
 	} else {
 		let waitingMeme = await meme();
 		memeId = waitingMeme[0].id;
-		await console.log(`from false part ${memeId} ${oldMeme}`);
+		await console.log(
+			`This was already posted. Current ID: ${memeId} Old ID: ${oldMeme}`
+		);
 
-		console.log("waiting from index.js FALSE PART");
-		await sleep(1800000);
-		console.log("30 minutes later");
+		await sleep(3600000);
+		console.log("60 minutes later");
 
 		alreadyPosted();
 	}
-	console.log(`from oustside part ${memeId} ${oldMeme}`);
 };
 
+// Start infinite loop
 alreadyPosted();
 
 // makeItHappen();
-
-// setInterval(function() {
-// 	makeItHappen();
-// }, 43200000);
